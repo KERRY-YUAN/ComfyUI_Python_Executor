165e40f56adcce6248dbe6b56764fd206995b821
Kerry <399237818@qq.com>
2025-04-01 15:57:15 +0800

    Add node_auto_sampler

--- __init__.py
+++ __init__.py
@@ -1,17 +1,20 @@
 from .node_python_executor import NodePythonExecutor
 from .node_resize_image import NodeResizeImage
+from .node_auto_sampler import NodeAutoSampler
 
 # --- Node Registration / 节点注册 ---
 NODE_CLASS_MAPPINGS = {
     # Mapping key and class name / 映射键和类名
     "NodePython": NodePythonExecutor,
-    "NodeResizeImage": NodeResizeImage
+    "NodeResizeImage": NodeResizeImage,
+    "NodeAutoSampler": NodeAutoSampler
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
      # Display name / 显示名称
     "NodePython": "NodePython",
-    "NodeResizeImage": "NodeResizeImage"
+    "NodeResizeImage": "NodeResizeImage",
+    "NodeAutoSampler": "NodeAutoSampler"
 }
 
 __all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS",]
